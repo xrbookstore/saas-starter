de6bd1a713c6189f579f830ee4f33768a2eee2c4
tomoki <xrbookstore.dev@gmail.com>
2025-12-19 16:43:16 +0900

    refactor: update layout and TypeScript configuration; remove middleware and add proxy for session handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import './globals.css';
-import type { Metadata, Viewport } from 'next';
-import { Manrope } from 'next/font/google';
-import { getUser, getTeamForUser } from '@/lib/db/queries';
-import { SWRConfig } from 'swr';
+import "./globals.css";
+import type { Metadata, Viewport } from "next";
+import { Manrope } from "next/font/google";
+import { getUser, getTeamForUser } from "@/lib/db/queries";
+import { SWRConfig } from "swr";
 
 export const metadata: Metadata = {
-  title: 'Next.js SaaS Starter',
-  description: 'Get started quickly with Next.js, Postgres, and Stripe.'
+  title: "Next.js SaaS Starter",
+  description: "Get started quickly with Next.js, Postgres, and Stripe.",
 };
 
 export const viewport: Viewport = {
-  maximumScale: 1
+  maximumScale: 1,
 };
 
-const manrope = Manrope({ subsets: ['latin'] });
+const manrope = Manrope({ subsets: ["latin"] });
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
@@ -24,6 +24,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
+      suppressHydrationWarning={true}
     >
       <body className="min-h-[100dvh] bg-gray-50">
         <SWRConfig
@@ -31,9 +32,9 @@ export default function RootLayout({
             fallback: {
               // We do NOT await here
               // Only components that read this data will suspend
-              '/api/user': getUser(),
-              '/api/team': getTeamForUser()
-            }
+              "/api/user": getUser(),
+              "/api/team": getTeamForUser(),
+            },
           }}
         >
           {children}
